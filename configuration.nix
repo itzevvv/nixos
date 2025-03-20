b804ecc474ba3576b720b1a0f199d6bf86425a4a
itzevvv <76193592+itzevvv@users.noreply.github.com>
2025-03-20 02:47:09 -0500

    Added gparted

--- configuration.nix
+++ configuration.nix
@@ -118,6 +118,7 @@
      keepassxc
      git
      github-desktop
+     gparted
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
